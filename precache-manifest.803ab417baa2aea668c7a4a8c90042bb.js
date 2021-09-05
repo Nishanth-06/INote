ce83f60a0442ddaf1335d6f6962ccad18c56744f
Nishanth-06 <67260068+Nishanth-06@users.noreply.github.com>
2021-09-05 10:38:47 +0530

    Updates

---
+++ precache-manifest.803ab417baa2aea668c7a4a8c90042bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5f2a47e91f291005ec66df6b1c3247",
+    "url": "/INote/index.html"
+  },
+  {
+    "revision": "c6e0529db4eab1e56aa2",
+    "url": "/INote/static/js/2.d2922420.chunk.js"
+  },
+  {
+    "revision": "2a3ffcc8259283681c62",
+    "url": "/INote/static/js/main.42348276.chunk.js"
+  },
+  {
+    "revision": "f01f49d8d240940c62f3",
+    "url": "/INote/static/js/runtime-main.5cb669dd.js"
+  }
+]);
