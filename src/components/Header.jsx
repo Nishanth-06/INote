57dc5708dd54490aaa93764cc85f4ce3ab186014
Nishanth-06 <67260068+Nishanth-06@users.noreply.github.com>
2021-09-05 10:26:16 +0530

    added git

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventNoteIcon from '@material-ui/icons/EventNote';
+
+function Header() {
+  return (
+    <header>
+       <h1><EventNoteIcon/>INote</h1>
+    </header>
+  );
+}
+
+export default Header;
